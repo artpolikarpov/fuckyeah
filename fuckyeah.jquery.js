9802f745eb87cb60770fc73ceca8a5091108f676
Sergey Ponomarev <stokito@gmail.com>
2012-11-23 09:52:08 +0200

    Update fuckyeah.jquery.js

--- fuckyeah.jquery.js
+++ fuckyeah.jquery.js
@@ -1,6 +1,7 @@
 /*! FuckYeah v1.0 https://github.com/artpolikarpov/fuckyeah | MIT LICENSE */
 (function ($) {
   $.fuckYeah = $.fn.fuckYeah = function () {
+    concole.log('FUCK YEEAAAHHH!!!');
     return this;
   }
 })(jQuery);
